707ee0058e4b86716a0cb9a187ebd4899426b4f8
Tobitap04 <ttappe@gmx.net>
2024-11-15 10:41:18 +0100

    Initial Commit

---
+++ iosFilmApp/iosFilmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosFilmAppApp.swift
+//  iosFilmApp
+//
+//  Created by Tobias Tappe on 15.11.24.
+//
+
+import SwiftUI
+
+@main
+struct iosFilmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosFilmAppTests/iosFilmAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosFilmAppTests.swift
+//  iosFilmAppTests
+//
+//  Created by Tobias Tappe on 15.11.24.
+//
+
+import Testing
+@testable import iosFilmApp
+
+struct iosFilmAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
